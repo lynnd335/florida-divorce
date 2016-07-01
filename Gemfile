078f1d102878ebb025ed2a69842f031b5fce101a
Dan Lynn <lynnd335@gmail.com>
2016-07-01 11:27:01 -0400

    2nd commit

--- Gemfile
+++ Gemfile
@@ -27,10 +27,14 @@ gem 'rtrain', '~> 0.2.8'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+group :production do
+  # gem 'pg'
+  gem 'unicorn'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,6 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,19 +8,17 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :rails_env, 'production'
-set :domain, 'myserver.com' 
-set :deploy_to, '/home/myuser/myserver.com' 
-set :repository, 'https://github.com/my-git-user/my-repo.git'
+set :domain, '159.203.75.141'
+set :user, 'florida_divorce'
+set :deploy_to, "/home/florida_divorce/app"
+set :repository, 'git@github.com:lynnd335/florida-divorce.git'
 set :branch, 'master'
-
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
 
+set :term_mode, :nil
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
@@ -31,28 +27,21 @@ set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  ruby_version = File.read('.ruby-version').strip
+  raise "Couldn't determine Ruby version: Do you have a file .ruby-version in your project root?" if ruby_version.empty?
+  queue %{
+    source /etc/profile.d/rvm.sh
+    rvm use #{ruby_version} || exit 1
+  }
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
-
+  
+  # Add the repository server to .ssh/known_hosts
   if repository
     repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
     repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
@@ -63,6 +52,26 @@ task :setup => :environment do
       fi
     ]
   end
+  
+  # Create database.yml for Postgres if it doesn't exist
+  path_database_yml = "#{deploy_to}/#{shared_path}/config/database.yml"
+  database_yml = %[production:
+  database: #{user}
+  adapter: postgresql
+  pool: 5
+  timeout: 5000]
+  queue! %[ test -e #{path_database_yml} || echo "#{database_yml}" > #{path_database_yml} ]
+  
+  # Create secrets.yml if it doesn't exist
+  path_secrets_yml = "#{deploy_to}/#{shared_path}/config/secrets.yml"
+  secret = 
+  secrets_yml = %[production:
+  secret_key_base:
+    #{`rake secret`.strip}]
+  queue! %[ test -e #{path_secrets_yml} || echo "#{secrets_yml}" > #{path_secrets_yml} ]
+  
+  queue! %[chmod g+rx,u+rwx,o-rwx "#{deploy_to}/#{shared_path}/config"]
+
 end
 
 desc "Deploys the current version to the server."
@@ -81,8 +90,7 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      #queue "sudo service #{user} restart"
     end
   end
 end
